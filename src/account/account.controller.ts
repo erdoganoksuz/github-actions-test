bdcc5f68713b03c6fca2b0f3429b3a235d64e0c0
Erdogan Oksuz <erdoganoksuz95@gmail.com>
2024-02-20 11:30:49 +0400

    lint fix
    Signed-off-by: Erdogan Oksuz <erdoganoksuz95@gmail.com>

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,23 +1,13 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Patch, Param, Delete } from '@nestjs/common';
 import { AccountService } from './account.service';
-import { CreateAccountDto } from './dto/create-account.dto';
-import { UpdateAccountDto } from './dto/update-account.dto';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
 
   @Post()
-  create(@Body() createAccountDto: CreateAccountDto) {
-    return this.accountService.create(createAccountDto);
+  create() {
+    return this.accountService.create();
   }
 
   @Get()
@@ -31,8 +21,8 @@ export class AccountController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAccountDto: UpdateAccountDto) {
-    return this.accountService.update(+id, updateAccountDto);
+  update(@Param('id') id: string) {
+    return this.accountService.update(+id);
   }
 
   @Delete(':id')
